b1da09a0112f56b8d6681aaeb8922373fcd08d99
Phakeo Arounerangsy <kronomon1@uri.edu>
2023-07-27 11:14:00 -0400

    new repo

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import cv2
 
-def display_camera():
+def display_camera_with_facial_features():
+    # Load the pre-trained Haar Cascade classifiers for face and eyes detection
+    face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+    eye_cascade = cv2.CascadeClassifier("haarcascade_eye.xml")
+
     # Open the default camera (usually the first camera in the system)
     cap = cv2.VideoCapture(0)
 
@@ -16,8 +20,27 @@ def display_camera():
             print("Error: Unable to capture frame.")
             break
 
-        # Display the frame
-        cv2.imshow("Camera Footage", frame)
+        # Convert the frame to grayscale (facial recognition requires grayscale images)
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Detect faces in the frame
+        faces = face_cascade.detectMultiScale(gray_frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+        # Draw rectangles around the detected faces and eyes
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+            # Region of Interest (ROI) within the face for eyes detection
+            roi_gray = gray_frame[y:y + h, x:x + w]
+            roi_color = frame[y:y + h, x:x + w]
+
+            # Detect eyes in the ROI
+            eyes = eye_cascade.detectMultiScale(roi_gray)
+            for (ex, ey, ew, eh) in eyes:
+                cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (255, 0, 0), 2)
+
+        # Display the frame with facial features
+        cv2.imshow("Camera Footage with Facial Features", frame)
 
         # Wait for 1 millisecond and check if 'q' key is pressed to exit
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -28,4 +51,4 @@ def display_camera():
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    display_camera()
+    display_camera_with_facial_features()
